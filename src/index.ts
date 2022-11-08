ef5eb4e92a283b40015b36eb876ba0fb894da9ec
Mrlolpro <leonardoollero14@gmail.com>
2022-11-08 20:05:00 +0100

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
+#!/usr/bin/env node
+/* 
+          _ _   _     
+         | (_) (_)    
+ ___  ___| |_   _ ___ 
+/ __|/ __| | | | / __|
+\__ \ (__| | |_| \__ \
+|___/\___|_|_(_) |___/
+              _/ |    
+             |__/     
+
+*/
+
+// MODULES
 import chalk from 'chalk';
 import inquirer from 'inquirer';
-import chalkAnimation from 'chalk-animation';
 import fs from 'fs';
 import exec from 'exec-sh';
 import { createSpinner } from 'nanospinner';
+// FUNCTIONS
+import packageManagerSelector from './selectors/packagemanager.selector';
+
+// VARIABLES
+var packageManager: 'npm' | 'yarn' | 'pnpm' = 'pnpm';
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+(async () => {
+  packageManager = await packageManagerSelector();
+})();
