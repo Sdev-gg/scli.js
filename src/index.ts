dd70e96b50760d18a17e9cb0c15073ef8bfe3f8e
MrlolDev <65485999+MrlolDev@users.noreply.github.com>
2022-11-08 12:19:39 +0000

    base

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import chalkAnimation from 'chalk-animation';
+import fs from 'fs';
+import exec from 'exec-sh';
+import { createSpinner } from 'nanospinner';
